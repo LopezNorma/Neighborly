8c4eef7edcb158ed920686238c2a9f41f742c586
jesp7 <121585767+jesp7@users.noreply.github.com>
2023-08-23 19:39:28 -0700

    finalized routes and database

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask_app import app 
 from flask_app.controllers import user_controller #replace rename_controller with your controller file name. Do not include .py
-from flask_app.controllers import helpme_controller
+from flask_app.controllers import helpmes
 
 
 if __name__=='__main__':
